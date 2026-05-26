4da4422249d14692fd63ffa40b3800fe5557be13
IgorStarnowski <istarnowski@gmail.com>
2026-05-26 14:08:08 +0200

    4

--- Sklep/src/main/java/com/igor/sklep/controller/HomeController.java
+++ Sklep/src/main/java/com/igor/sklep/controller/HomeController.java
@@ -1,21 +1,46 @@
 package com.igor.sklep.controller;
 
+import com.igor.sklep.model.Item;
 import com.igor.sklep.repository.ItemRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
     private final ItemRepository itemRepository;
+
     @Autowired
-    public HomeController(ItemRepository itemRepository){
+    public HomeController(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
     }
+
     @GetMapping("/")
-    public String home(Model model){
+    public String home(Model model) {
         model.addAttribute("items", itemRepository.findAll());
         return "home";
+    }
+
+    @GetMapping("/add/{itemId}")
+    public String addItemToCart(@PathVariable("itemId") Long itemId, Model model, HttpSession session){
+        @SuppressWarnings("unchecked")
+        List<Item> cart = (List<Item>) session.getAttribute("cart");
+        if(cart==null){
+            cart=new ArrayList<>();
+        }
+        Optional<Item> oItem = itemRepository.findById(itemId);
+        if(oItem.isPresent()){
+            Item item = oItem.get();
+            cart.add(item);
+            session.setAttribute("cart", cart);
+        }
+        return "redirect:/";
     }
 }
